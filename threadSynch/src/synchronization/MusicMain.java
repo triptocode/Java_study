14a7a66f0204c67b8cf1e2acef16b6303f3e45f9
leticia <leticia@192.168.200.147>
2021-06-21 16:19:40 +0900

    extendsThread / Runnable/ synchronized

--- threadSynch/src/synchronization/MusicMain.java
+++ threadSynch/src/synchronization/MusicMain.java
@@ -6,7 +6,7 @@ public class MusicMain {
 		
 	Music musicObj = new Music();
 		
-	PlayerThread player1 = new PlayerThread(1, musicObj);
+	PlayerThread player1 = new PlayerThread(1, musicObj);  // °øÀ¯°´Ã¼  Music 
 	PlayerThread player2 = new PlayerThread(1, musicObj);
 	PlayerThread player3 = new PlayerThread(3, musicObj);
 		
