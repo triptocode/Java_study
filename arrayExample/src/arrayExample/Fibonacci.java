79b0072b28251220d5b1e9aa6e6d5e6dfa9bdb89
triptocode <3580rio@gmail.com>
2021-07-04 12:54:51 +0900

    int[] a={} /{''}/ char[]a ASCII/ hash/Fibonacci

---
+++ arrayExample/src/arrayExample/Fibonacci.java
@@ -0,0 +1,34 @@
+package arrayExample;
+
+public class Fibonacci {
+	
+
+	
+	public void fibonacci() {
+		int[] arr = new int[10];
+		
+		for(int i=0; i < arr.length; i++) {
+			if(i <= 1) {
+				arr[i] = 1;
+			} else {		// i > 2 인 경우
+				arr[i] = arr[i - 1] + arr[i - 2];
+			}
+			
+			for(int value : arr) {    // 요소의 타입변수 : 배열 또는 컬렉션
+				System.out.println(value);
+			}
+		}
+		
+	}	
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			Fibonacci fibo = new Fibonacci();
+			fibo.fibonacci();
+		}
+
+}
+	
+
+
