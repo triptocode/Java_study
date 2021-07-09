0bff352548a88fa3a7381191cfc7d6a1025e0da5
triptocode <3580rio@gmail.com>
2021-07-09 14:18:34 +0900

    System.out.printf=System.out.format/ %tp %tI:%tM %n %10.3f

--- foramt/src/basic02/Formact.java
+++ foramt/src/basic02/Formact.java
@@ -9,15 +9,16 @@ public class Formact {
 	      
 			// System.out.printf()=System.out.format()
 			// %n 줄바꿈  
-			// 월: tb, tm  
+			// 월: %tB, %tm  
+		 
 			 Calendar c = Calendar.getInstance();  
-		      System.out.printf("%tm%n", c); 
+		      System.out.printf("%tm%n", c); // 07
 		      System.out.format("%tm%n", c); // 07
 		      
 		      System.out.format("%tB%n", c); // 7월
 		      System.out.format("%tB %te, %tY%n", c, c, c); // --> 7월 9, 2021 / (현재 월,일,연도)
-		      System.out.format("%tp%tI:%tM%n", c, c, c);  // -->오전11:03  / (현재시간)
-		      System.out.format("%tD%n", c);    // -->  07/09/21  / (현재 월,일,연도)
+		      System.out.format("%tp %tI:%tM %n", c, c, c);  // -->오전 11:03  / (현재시간)
+		      System.out.format("%tD%n%n", c);    // -->  07/09/21  / (현재 월,일,연도)
 			 
 			 long n = 123456;
 		       
@@ -31,13 +32,10 @@ public class Formact {
 		      double pi = Math.PI;
 
 		      System.out.format("%f%n", pi);       // -->  3.141593
-		      System.out.format("%.3f%n", pi);     // -->  3.142
-		      System.out.format("%10.3f%n", pi);   // -->       3.142
+		      System.out.format("%.3f%n", pi);     // -->  3.142       / .3 은 소수점자리이하 3개를 의미 
+		      System.out.format("%10.3f%n", pi);   // -->       3.142  / 앞 공백5개, 뒤에 . 포인트 포함 5개까지 총10자리
 		      System.out.format("%-10.3f%n", pi);  // -->  3.142
 		      System.out.format(Locale.FRANCE,
 		                        "%-10.4f%n%n", pi); // -->  3,1416
-	     
-		    }
-	
-	
+	 }	
 }
