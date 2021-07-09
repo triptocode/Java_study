6b4c99bd987e3a1b9361bc627f635fe7e6ec7baf
triptocode <3580rio@gmail.com>
2021-07-09 12:58:17 +0900

    Constructor format MethodReturn

--- basic/src/basic/Format.java
+++ basic/src/basic/Format.java
@@ -8,7 +8,7 @@ public class Format {
 	      
 		// System.out.printf()=System.out.format()
 		// %n 줄바꿈 
-		// 월: tb, tm 
+		// 월: tb, tm
 		 Calendar c = Calendar.getInstance();  
 	      System.out.printf("%tB%n", c); 
 	      System.out.format("%tm%n", c); // 07
